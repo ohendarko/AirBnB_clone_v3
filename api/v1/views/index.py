dd61569666906413bb94c2e572663750db44ea91
ohendarko <ohendarko@gmail.com>
2024-02-24 19:45:00 +0000

    submit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 from api.v1.views import app_views
 
 
-@app_views.route("/status")
+@app_views.route("/status", methods=['GET'])
 def status():
     """return OK status"""
     return {"status": "OK"}
