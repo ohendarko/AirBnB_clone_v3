6fb847610f8366d461ba93ab27be85f29e230d0d
ohendarko <ohendarko@gmail.com>
2024-02-24 20:57:11 +0000

    submit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """Pending documentation"""
 from api.v1.views import app_views
+from models.engine import db_storage
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", methods=['GET'])
@@ -11,5 +19,10 @@ def status():
 
 @app_views.route("/stats", methods=['GET'])
 def stats():
-    """return OK status"""
-    return {"status": "OK"}
+    """ retrieves the number of each objects by type"""
+    klass = db_storage.classes
+    counts = {}
+    for key in klass:
+        value = klass[key]
+        counts[key.lower()] = storage.count(value)
+    return counts
