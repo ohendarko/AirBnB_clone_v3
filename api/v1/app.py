40dbddae9a935ca7b35e82f47ee1d7aa1a499a0a
ohendarko <ohendarko@gmail.com>
2024-02-24 20:14:59 +0000

    submit

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 
 app = Flask(__name__)
@@ -16,4 +17,6 @@ def remove_session(exc):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
